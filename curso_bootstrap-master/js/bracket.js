1e678c7108ca466cfc02c43e6c42b4a6169ef0b5
ed-science <edsg.eds@gmail.com>
2023-03-02 22:11:38 -0300

    atualizacao conclusao 20230302

---
+++ curso_bootstrap-master/js/bracket.js
@@ -0,0 +1,27 @@
+if (testes){
+    if (testes2){
+        if (testes3){
+
+        }
+
+    }
+
+}
+
+let obj = {
+    'car' : ['BMW', 'CHEVROLET'],
+    'casa': ['2 quartos', '4 quartos',[]],
+
+}
+
+
+let a      = 0;                  //   align test extension
+let nome   = 'edson';            // more comments
+let rua    = 'avenue street 2';
+let number = 31;
+
+/**
+ * ? Test better comments
+ * ! warning codes
+ * @param quote rotation
+ */
---
+++ curso_bootstrap-master/js/es6snippets.js
@@ -0,0 +1,19 @@
+export default class {
+    
+};
+
+console.log(object);
+
+console.trace(object);
+
+const name = (params) => {
+    
+};
+
+console.log(object)
+
+console.error
+
+
+
+let code
